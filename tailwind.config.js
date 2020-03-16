5ae08585533b1182a708d81ba547d8285bf9361b
Nic Goodman <nicgoodman@Nics-MBP.lan>
2020-03-16 17:52:26 -0400

    added custom fonts and installed plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {colors: {
+      'wz-black': '#212A30',
+      'wz-cream': '#FEF4E9',
+      'wz-yellow': '#F2C58C',
+      'wz-dark-teal': '#527286',
+      'wz-teal': '#65869C',
+      'wz-dark-red': '#8E1A22',
+      'wz-red': '#AB2739',
+    }
   },
+  fontFamily: {
+    'wz-italic': ['SF WADIM GIANT ITALIC, sans-serif'],
+    'wz-lines': ['SF WADIM GIANT LINES, sans-serif'],
+    'wz-outline': ['SF WADIM GIANT OUTLINE, sans-serif'],
+    'wz-heavy': ['SF WADIM GIANT Heavy, sans-serif'],
+  }
+},
   variants: {},
   plugins: [],
 }
