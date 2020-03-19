2735402bdc2af038cfa62469111131a75f3a4fed
Nic Goodman <nicgoodman@Nics-MBP.lan>
2020-03-19 16:06:48 -0400

    added vue.js basic install

---
+++ web/assets/js/app.js
@@ -0,0 +1,28 @@
+const router = new VueRouter({
+  routes: [{
+    path: '/',
+    component: Home
+  }, {
+    path: '/parts',
+    component: Parts
+  }, {
+    path: '/pre-owned',
+    component: Pre-Owned
+  }, {
+    path: '/rentals',
+    component: Rentals
+  }, {
+    path: '/salvage',
+    component: Salvage
+  }, {
+    path: '/service',
+    component: Service
+  }]
+}) const app = new Vue({
+  el: '#main',
+  router
+})
+
+const Home = {
+  template: `<content></content>`
+}
---
+++ web/assets/js/components.js
@@ -0,0 +1,6 @@
+Vue.component('content', {
+  template: `  <section id="content">
+  <p>test</p>
+  <p>{{ entry.testContent }}</p>
+  </section>`
+})
