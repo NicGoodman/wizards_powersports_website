f8a844400d27f3666823dc19461fea9a356d4c9c
Nic Goodman <nicgoodman@Nics-MBP.lan>
2020-03-19 16:17:16 -0400

    added variables

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -2,6 +2,26 @@ const Home = {
   template: `<main-content></main-content>`
 }
 
+const Parts = {
+  template: `<main-content></main-content>`
+}
+
+const PreOwned = {
+  template: `<main-content></main-content>`
+}
+
+const Rentals = {
+  template: `<main-content></main-content>`
+}
+
+const Salvage = {
+  template: `<main-content></main-content>`
+}
+
+const Service = {
+  template: `<main-content></main-content>`
+}
+
 const router = new VueRouter({
   routes: [{
     path: '/',
@@ -11,7 +31,7 @@ const router = new VueRouter({
     component: Parts
   }, {
     path: '/pre-owned',
-    component: Pre-Owned
+    component: PreOwned
   }, {
     path: '/rentals',
     component: Rentals
